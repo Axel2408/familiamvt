5319691adb7ea24403f1a9d43ae68e72665979cb
Axel2408 <axelrubenacker46@gmail.com>
2022-04-04 19:46:15 -0300

    Initial commit

---
+++ familiamvt/views.py
@@ -0,0 +1,31 @@
+from multiprocessing import context
+from re import template
+from django.http import HttpResponse
+from django.template import Template, context, loader
+
+def primervsita(request):
+    return HttpResponse ("Familia!")
+
+def segundavista(request):
+    return HttpResponse("listado de mi familia")
+
+def pagina_inicio(request):
+    archivo=open(r"C:\Users\PC\Desktop\PYTHON\famiia env\familiamvt\familiamvt\template\familia.html", "r")
+    
+    nombreyparentezco={"papa":"Alexis", "mama":"Andrea", "hermano":"Audric", "tio":"Claudio"}
+
+    cumpleaños={"papa":"06/04/1972","mama": "05/06/1972", "hermano":"28/03/2007", "tio":"12/02/1971"}
+
+    edad={"papa":49, "mama":49, "hermano":15, "tio":51}
+
+    dic_context={"nombreypa": nombreyparentezco, "cumple":cumpleaños, "edad":edad}
+
+    plantilla=Template(archivo.read())
+
+    archivo.close()
+
+    contexto=context(dic_context)
+
+    documento=plantilla.render(contexto)
+
+    return HttpResponse(documento)
